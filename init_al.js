9c9127c19d4d47a8b789fb30f2de53fc0a194e37
AlanLozada64 <alanlozada2001@gmail.com>
2024-04-29 08:25:08 -0500

    js

--- init_al.js
+++ init_al.js
@@ -2,3 +2,10 @@
 function mensaje(){
     alert("Hola Mundo");
 }
+
+function calcularImpuesto(valor){
+    var iva = valor * 15 / 100;
+    var total = valor + iva;
+    alert(total);
+}
+calcularImpuesto(100);
