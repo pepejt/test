13baa7341378c8ac4430f87f8785f34e75827d54
AnthonyGuti <3btigutierrezanthony@gmail.com>
2024-04-29 08:33:47 -0600

    Cambios en el html y añadido mas informacion en el js

--- init_ga.js
+++ init_ga.js
@@ -1,3 +1,26 @@
 function mensaje(){
     alert("Hola mundo");
 }
+
+function calcularImpuesto(valor){ 
+    var iva = valor * 15 / 100; 
+    var total = valor + iva; 
+    alert (total);
+    return total;
+}
+/*comentario
+calcularImpuesto(100);*/
+
+var txtvalor = document.getElementById('txtvalor');
+var btnAceptar = document.getElementById('btnAceptar');
+var divresultado  = document.getElementById('divresultado')
+btnAceptar.onclick=function(){
+    var valor = txtvalor.value;
+    if(valor === ""){
+        valor = 0;
+    }else{
+        valor = valor*1;
+    }
+    var resultado=calcularImpuesto(valor);
+    divresultado.innerHTML =resultado;
+}
