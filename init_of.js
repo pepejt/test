25aaf4487dbb242698d9a0cbec2c3b5d76209c11
Fernando-8a@github.com <fochoa2@indoamerica.edu.ec>
2024-04-29 09:35:15 -0500

    Cambios de js y htmml

--- init_of.js
+++ init_of.js
@@ -1,5 +1,30 @@
 
 function mensaje(){
     alert("SapO");
-
+}
+/* 
+-CamelCase
+-Pascal
+-Kamba
+ */
+function calcularImpuesto(valor){
+    var iva =  valor * 15 / 100;
+    var total = valor + iva;
+    
+    return total;
+}
+/*Comentario */
+//calcularImpuesto(100);
+var txtValor = document.getElementById("txtValor");
+var btnAceptar = document.getElementById("btnAceptar");
+var divResultado =  document.getElementById("divResultado");
+btnAceptar.onclick =  function (){
+    var valor = txtValor.value;
+    if(valor === ""){
+        valor = 0;
+    }else{
+        valor = valor*1;
+    }
+    var resultado = calcularImpuesto(valor);
+    divResultado.innerHTML = resultado+ " ";
 }
