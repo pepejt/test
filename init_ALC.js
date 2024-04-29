266fae7c262024afb6dfcd4ba27646abdf8be925
Andres <alopez49@indoamerica.edu.ec>
2024-04-29 09:58:56 -0500

    Actualizacion de codigo clase 29-04-2024

--- init_ALC.js
+++ init_ALC.js
@@ -13,6 +13,7 @@ function calcularImpuesto(valor){
 var txtValor = document.getElementById("txtValor");
 var btnAceptar = document.getElementById("btnAceptar");
 var divResultado = document.getElementById("divResultado");
+var imgLogo = document.getElementById("imgLogo");
 
 btnAceptar.onclick = function(){
     var valor = txtValor.value;
@@ -23,7 +24,14 @@ btnAceptar.onclick = function(){
     }   
     
     var resultado = calcularImpuesto(valor); 
+    if (resultado > 100){
+        divResultado.className= "exito";
+        imgLogo.src = "https://i.pinimg.com/564x/2b/64/b7/2b64b7e80396146f4e1a9865e234ee36.jpg"
+
+    }else{
+        divResultado.className= "error";
+    }
     divResultado.innerHTML = resultado;
-       
+
 
 }
