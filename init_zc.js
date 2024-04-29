389b219cfca1f132513daa8c810643c3b784b7a6
CZurita14 <czurita14@indoamerica.edu.ec>
2024-04-29 09:59:20 -0500

    Actualizacion de codigos html, css y java 29/04/2024

--- init_zc.js
+++ init_zc.js
@@ -11,6 +11,7 @@ function calcularImpuesto(valor){
 var txtValor = document.getElementById('txtValor');
 var btnAceptar = document.getElementById('btnAceptar');
 var divResultado = document.getElementById('divResultado');
+var imgLogo = document.getElementById('imgLogo');
 
 btnAceptar.onclick = function(){
     var valor = txtValor.value;
@@ -20,6 +21,12 @@ btnAceptar.onclick = function(){
         valor = valor * 1;
     }
     var resultado = calcularImpuesto(valor);
+    if (resultado > 100){
+        divResultado.className = 'exito';
+        imgLogo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeBCSMFRJAsa-GTcsTnr6G2aiojWSi0YsgOSpaiR1G3A&s"
+    }else{
+        divResultado.className = 'error';
+    }
     divResultado.innerHTML = resultado;
 }
 
