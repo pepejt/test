473279b604a0d36c9cc2a7c52c97d6428c819b86
PatrickZ29 <zpatrick350@gmail.com>
2024-04-29 09:30:34 -0500

    Actualizacion de mi Rama

--- init_zp.js
+++ init_zp.js
@@ -1,5 +1,29 @@
 
 function mensaje(){
     alert("Hola Mundo");
-    
+
+}
+
+
+function calcularImpuesto(valor){
+    var iva = valor * 15 /100;
+    var total = valor + iva;
+    alert (total);
+    return total;
+}
+//comentario//
+//calcularImpuesto(100);
+
+var txtValor = document.getElementById("txtValor");
+var btnAceptar = document.getElementById("btnAceptar");
+var divResultado = document.getElementById("divResultado");
+btnAceptar.onclick = function(){
+    var valor = txtValor.value;
+    if (valor === ""){
+valor = 0;
+    }else{
+        valor = valor*1;
+    }
+    var resultado = calcularImpuesto(valor);
+    divResultado.innerHTML = resultado;
 }
