20e013181d86b81d4ca8b31c9b56d2992bb0705b
Andres <alopez49@indoamerica.edu.ec>
2024-04-29 09:31:09 -0500

    Actualizacion de codigo clase 29/09/2024

--- init_ALC.js
+++ init_ALC.js
@@ -1,5 +1,29 @@
 function mensaje(){
     alert("Hola inge");
 }
+/*CamelCase-Pascal-kamba*/
+function calcularImpuesto(valor){
+    var iva = valor * 15 / 100;
+    var total = valor + iva;
+    return total;
+}
 
+//calcularImpuesto(100);
 
+var txtValor = document.getElementById("txtValor");
+var btnAceptar = document.getElementById("btnAceptar");
+var divResultado = document.getElementById("divResultado");
+
+btnAceptar.onclick = function(){
+    var valor = txtValor.value;
+    if(valor === ""){
+        valor = 0;
+    }else{
+        valor = valor * 1;
+    }   
+    
+    var resultado = calcularImpuesto(valor); 
+    divResultado.innerHTML = resultado;
+       
+
+}
