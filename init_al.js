096e89c63dec7928e45bbcc86fc1e76e599984e3
AlanLozada64 <alanlozada2001@gmail.com>
2024-04-29 09:30:22 -0500

    agregado init

--- init_al.js
+++ init_al.js
@@ -6,6 +6,21 @@ function mensaje(){
 function calcularImpuesto(valor){
     var iva = valor * 15 / 100;
     var total = valor + iva;
-    alert(total);
+    return total;
 }
-calcularImpuesto(100);
+//calcularImpuesto(100);
+
+var txtVaalor = document.getElementById("txtValor");
+var btnAceptar = document.getElementById("btnAceptar");
+var divResultado = document.getElementById("divResultado");
+
+btnAceptar.onclick = function(){
+    var valor = txtValor.value;
+    if (valor === ""){
+        valor = 0;
+    }else{
+        valor = valor * 1;
+    }
+    var resultado = calcularImpuesto(valor);
+    divResultado.innerHTML = resultado;
+}
