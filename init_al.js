7df15b7d0f161adcac46e45d7f3fd06c686e73e8
AlanLozada64 <alanlozada2001@gmail.com>
2024-04-29 09:59:04 -0500

    actualizado

--- init_al.js
+++ init_al.js
@@ -13,6 +13,7 @@ function calcularImpuesto(valor){
 var txtVaalor = document.getElementById("txtValor");
 var btnAceptar = document.getElementById("btnAceptar");
 var divResultado = document.getElementById("divResultado");
+var imgLOGO = document.getElementById("imgLOGO");
 
 btnAceptar.onclick = function(){
     var valor = txtValor.value;
@@ -22,5 +23,11 @@ btnAceptar.onclick = function(){
         valor = valor * 1;
     }
     var resultado = calcularImpuesto(valor);
+    if(resultado > 100){
+        divResultado.className = "exito";
+        imgLOGO.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/768px-LEGO_logo.svg.png"
+    }else{
+        divResultado.className = "error";
+    }
     divResultado.innerHTML = resultado;
 }
