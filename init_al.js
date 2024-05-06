dc2a04a8cb7012f3e487e647b00f77aa749de4c9
AlanLozada64 <alanlozada2001@gmail.com>
2024-05-06 09:50:31 -0500

    Agregaddo json

--- init_al.js
+++ init_al.js
@@ -31,3 +31,50 @@ btnAceptar.onclick = function(){
     }
     divResultado.innerHTML = resultado;
 }
+
+
+//Definicion tipo json
+/*var estudiantes = [
+{cedula:"1804497343",nombre:"Alan",apellido:"Lozada"},
+{cedula:"1803728152",nombre:"Kevin",apellido:"Castillo"},
+{cedula:"1804497335",nombre:"Juan",apellido:"Perez"}
+];*/
+
+//console.log(estudiantes[2].apellido);
+/*for(i=0; i<estudiantes.length;i++){
+    console.log(estudiantes[i].nombre, estudiantes[i].apellido);
+}*/
+
+var tblEstudiante = document.getElementById("tblEstudiante");
+
+fetch("estudiantes_al.json").then(
+    (estudiantes)=>{
+        if (estudiantes.status == 200){
+            return estudiantes.json();
+
+        } 
+    }
+).then(
+    (estudiantes)=>{
+        
+        for(i=0; i<estudiantes.length; i++){
+            var row = tblEstudiante.insertRow(i+1);
+            var colCedula = row.insertCell(0);
+            colCedula.innerHTML = estudiantes[i].cedula;
+            var colNombre = row.insertCell(1);
+            colNombre.innerHTML = estudiantes[i].nombre + " " + estudiantes[i].apellido;
+        }    
+    }
+)
+
+
+
+var estudiante = {
+    cedula: "1804497343",
+    nombre: "Alan",
+    apellido:"Lozada",
+    edad: 22,
+    suldo: 180
+}
+
+console.log(estudiante.cedula);
