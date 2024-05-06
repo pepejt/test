c95bbb9cfb56f4559c6f62fd4353dbe6f1b9c78c
Andres <alopez49@indoamerica.edu.ec>
2024-05-06 09:50:24 -0500

    Actualización de codigo clase 6/05/2024

--- init_ALC.js
+++ init_ALC.js
@@ -35,3 +35,44 @@ btnAceptar.onclick = function(){
 
 
 }
+
+//Definicion de tipo Json
+/*var estudiantes = [  
+    {cedula: '1850631688',nombre: 'Andres', apellido:'Lopez'}, 
+    {cedula: '1950465478', nombre: 'Kevin', apellido:'Castillo'},
+    {cedula: '1850631498', nombre: 'Juan', apellido: 'Perez'}
+] 
+
+//console.log(estudiantes [0].apellido);
+for(i=0; i<estudiantes.length; i++){
+    console.log(estudiantes[i].nombre + ' ' +estudiantes[i].apellido);
+}*/
+
+var tblEstudiante = document.getElementById('tblEstudiante');
+
+fetch("estudiantesALC.json").then(
+    (estudiantes)=>{
+        if(estudiantes.status ==200){
+        return estudiantes.json();    
+    }
+    }
+).then(
+    (estudiantes)=>{
+        for(i=0; i<estudiantes.length; i++){
+            var row = tblEstudiante.insertRow(i+1);
+            var colcedula1 = row.insertCell(0);
+            var colcedula2 = row.insertCell(1);
+            colcedula1.innerHTML = estudiantes[i].cedula;
+            colcedula2.innerHTML = estudiantes[i].nombre+'  '+estudiantes[i].apellido;
+        }
+    }
+)
+var estudiante = {
+    id: '1850631688',
+    nombre: 'Andres',
+    apellido: 'Lopez',
+    edad: 23,
+    sueldo: 210.10 
+}
+
+console.log(estudiante.cedula);
