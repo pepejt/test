53ec8a4e4cc2a5d1306ce134befee0bb1dcf1e1d
Khayro0 <jairoenriqueteranalbuja2005@gmail.com>
2024-05-06 09:54:20 -0500

    Cambios en Todo :D

--- init_tj.js
+++ init_tj.js
@@ -33,3 +33,46 @@ btnAceptar.onclick = function(){
     }
     divResultado.innerHTML = resultado;
 }
+
+// Definicion de tipo json
+/*var estudiantes = [
+    {cedula: '1850392406', nombre: 'Jairo', apellido:'Teran'}, // 0
+    {cedula: '0000000000', nombre: 'Kevin', apellido:'Castillo'}, // 1
+    {cedula: '1111111111', nombre: 'Anthony', apellido:'Gutierrez'} // 2
+];
+
+// console.log(estudiantes[].apellido);
+for(i=0; i<estudiantes.length; i++){
+    console.log(estudiantes[i].nombre, estudiantes[i].apellido);
+}*/
+
+var tblEstudiante = document.getElementById('tblEstudiante');
+
+fetch("estudiantes_tj.json").then(
+    (estudiantes) => {
+        if (estudiantes.status == 200){
+            return estudiantes.json();
+        }
+    }
+).then(
+    (estudiantes) => {
+        for(i=0; i<estudiantes.length; i++){
+            var row = tblEstudiante.insertRow(i+1);
+            var colCedula = row.insertCell(0);
+            colCedula.innerHTML = estudiantes[i].cedula;
+            var colNombre = row.insertCell(1);
+            colNombre.innerHTML = estudiantes[i].nombre + ' ' + estudiantes[i].apellido;
+            }
+    }
+)
+
+
+var estudiante = {
+    cedula: '1850392406',
+    nombre: 'Jairo',
+    apellido: 'Teran',
+    edad: 19,
+    sueldo: 1540.65
+}
+
+console.log(estudiante.cedula);
