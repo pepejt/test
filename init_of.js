cd3308f230e37571b94a1b33b7617458c383f820
Fernando-8a@github.com <fochoa2@indoamerica.edu.ec>
2024-05-06 09:52:22 -0500

    Cambios en js y ampliacio de json 06/05/2024

--- init_of.js
+++ init_of.js
@@ -36,3 +36,49 @@ btnAceptar.onclick =  function (){
     }
     divResultado.innerHTML = resultado+ " ";
 }
+//  Defiinicion de tipo json
+/*var estudiantes =[
+ {cedula:"0750919227",nombre:"Fernando",apellido: "Ochoa"},//0
+ {cedula:"0750919228",nombre:"Jose",apellido: "Cobeña"},//1
+ {cedula:"0750929228",nombre:"Angel",apellido: "Diaz"},//2
+
+];*/
+//console.log(estudiantes[2].apellido);
+/*for(i=0; i<estudiantes.length; i++ ){
+    console.log(estudiantes[i].nombre, estudiantes[i].apellido);
+}*/
+var tblEstudiante = document.getElementById("tblEstudiante");
+
+fetch("estudiantes_of.json").then(
+    (estudiantes)=>{
+        if(estudiantes.status == 200){
+            return estudiantes.json();
+        }
+
+    }
+).then( 
+    (estudiantes) => {
+    for(i=0; i<estudiantes.length; i++ ){
+        var row =  tblEstudiante.insertRow(i+1);
+        var colCedula = row.insertCell(0);
+        colCedula.innerHTML = estudiantes[i].cedula;
+        var colNombre = row.insertCell(1);
+        colNombre.innerHTML = estudiantes[i].nombre+" "+ estudiantes[i].apellido;
+        }
+}
+)
+/*for(i=0; i<estudiantes.length; i++ ){
+    var row =  tblEstudiante.insertRow(i+1);
+    var colCedula = row.insertCell(0);
+    colCedula.innerHTML = estudiantes[i].cedula;
+    var colNombre = row.insertCell(1);
+    colNombre.innerHTML = estudiantes[i].nombre+" "+ estudiantes[i].apellido;
+}*/
+var estudiante = {
+    cedula: "0750919227",
+    nombre: "Fernando",
+    apellido: "Ochoa",
+    edad: 38,
+    sueldo: 666.6
+}
+console.log(estudiante.cedula);
