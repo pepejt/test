072ad53c45aebede6d12296bb1ae33dea4310643
Javier454 <kc573894@gmail.com>
2024-05-06 09:51:07 -0600

    clase terminada

--- init_kc.js
+++ init_kc.js
@@ -34,3 +34,44 @@ btnAceptar.onclick = function() {
 }
 divResultado.innerHTML =  resultado;
 }
+
+//Definicion de tipo json
+/*var estudiantes = [
+    {cedula:'050314354',nombre:'Kevin',apellido:'Castillo'},//0
+    {cedula:'0401196969',nombre:'Juan',apellido:'Perez'},//1
+    {cedula:'0502486004',nombre:'David',apellido:'Chicaiza'},//2    
+];*/
+/*console.log(estudiantes[2].apellido);
+for(i=0; i<estudiantes.length;i++){
+    console.log(estudiantes[i].nombre, estudiantes[i].apellido); 
+}*/
+var tblEstudiante = document.getElementById('tblEstudiante');
+
+fetch("estudiantes_jt.json").then(
+    (estudiantes)=>{
+        if(estudiantes.status == 200){
+            return estudiantes.json();
+        }     
+    }
+).then(
+    (estudiantes)=>{
+        for(i=0; i<estudiantes.length;i++){
+            var row = tblEstudiante.insertRow(i+1);
+            var colCedula = row.insertCell(0); 
+            colCedula.innerHTML = estudiantes[i].cedula; 
+            var colNombre = row.insertCell(1);
+            colNombre.innerHTML = estudiantes[i].nombre+' '+estudiantes[i].apellido;
+         }
+    }   
+)
+
+
+
+var estudiante = {
+    cedula:'0503143554',
+    nombre: 'Juan',
+    apellido: 'Perez', 
+    edad: 25,
+    sueldo: 666.6,
+}
+console.log(estudiante.cedula);
