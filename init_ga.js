4476fa20a0899c08d71c1cada25a12ac84bb4d98
AnthonyGuti <3btigutierrezanthony@gmail.com>
2024-05-06 08:51:46 -0600

    Añadido mas informacion y creacion de un json

--- init_ga.js
+++ init_ga.js
@@ -32,3 +32,47 @@ btnAceptar.onclick=function(){
     }
     divresultado.innerHTML =resultado;
 }
+//Definicion de tipo json
+/*var estudiantes =   [
+    {cedula:'1804905998', Nombre:'Anthony', Apellido:'Gutierrez'},
+    {cedula:'1820156487', Nombre:'Kevin', Apellido:'Castillo'},
+    {cedula:'1814257889', Nombre:'oChoa', Apellido:'fernandez'},
+];
+   
+//console.log(estudiantes[0].Apellido);
+
+for (i=0; i<estudiantes.length; i++){
+    console.log(estudiantes[i].Nombre, estudiantes[i].Apellido)
+    
+    
+}*/
+var tbestudiante = document.getElementById('tbestudiante');
+fetch("estudiantes_ga.json").then(
+    (estudiantes)=> {
+        if(estudiantes.status == 200){
+            return estudiantes.json();
+        }
+    }
+).then(
+    (estudiantes) => {
+        for (i=0; i<estudiantes.length; i++){
+            var row = tbestudiante.insertRow(i+1);
+            var colcedula = row.insertCell(0);
+            colcedula.innerHTML = estudiantes[i].cedula;
+            var colnombre = row.insertCell(1);
+            colnombre.innerHTML = estudiantes[i].Nombre+ ' ' +estudiantes[i].Apellido;
+            }
+    }
+)
+
+
+var estudiante = {
+    cedula:'1804905998',
+    Nombre:'Anthony',
+    Apellido:'Gutierrez',
+    edad: 19,
+    sueldo: 15.0,
+}
+console.log(estudiante.cedula);
+console.log(estudiante.Nombre);
+console.log(estudiante.Apellido);
