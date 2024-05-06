d7f4b1336579910a48b342f2f6c3a9286d9678da
CZurita14 <czurita14@indoamerica.edu.ec>
2024-05-06 09:51:02 -0500

    Archivo json, cambios en html, cambios en init clase 06/05/2024

--- init_zc.js
+++ init_zc.js
@@ -30,6 +30,46 @@ btnAceptar.onclick = function(){
     divResultado.innerHTML = resultado;
 }
 
+//definicion de tipo json
+/*var estudiantes = [
+    {cedula:'1804765236',nombre:'Christian',apellido:'Zurita'},//0
+    {cedula:'180475124',nombre:'Kevin',apellido:'Castillo'},//1
+    {cedula:'18045453',nombre:'Juan',apellido:'Perez'}//2
+]*/
+//console.log(estudiantes[2].apellido);
+/*for(i=0 ; i<estudiantes.length; i++ ){
+    console.log(estudiantes[i].nombre , estudiantes[i].apellido)
+}*/
+
+var tblEstudiante =document.getElementById('tblEstudiante'); 
+
+fetch("estudiantes_cz.json").then(
+    (estudiantes)=>{
+        if(estudiantes.status == 200 ){
+            return estudiantes.json();
+        }
+    }
+).then(
+    (estudiantes)=>{
+        for(i=0 ; i<estudiantes.length; i++){
+            var row = tblEstudiante.insertRow(i+1);
+            var colCedula = row.insertCell(0);
+            colCedula.innerHTML = estudiantes[i].cedula;
+            var colNombre = row.insertCell(1);
+            colNombre.innerHTML = estudiantes[i].nombre+' '+estudiantes[i].apellido;
+        }
+    }
+
+)
+
+var estudiante = {
+    cedula:'1804765236',
+    nombre:'Chirstian',
+    apellido:'Zurita',
+    edad: 24,
+    sueldo: 110.10
+}
+console.log(estudiante.cedula);
 
 
 
